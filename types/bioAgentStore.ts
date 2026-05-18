d843941d44cfcca7b70b38742e8c3b12a67487df
서진교 <oygnijoes0209@gmail.com>
2026-05-18 17:27:30 +0900

    Revert "Merge pull request #25 from BigBioAI/feature/#24-process-ux"
    This reverts commit 1ff919e7f71d8a4c000e69a89ccd4493c72377f6, reversing
    changes made to fe50aa673afc46cc4f7992bf073c04f7067b157f.

--- types/bioAgentStore.ts
+++ types/bioAgentStore.ts
@@ -3,23 +3,6 @@ import { BioAgentMessage } from '@/types/chat';
 
 export type BioAgentPhase = 'upload' | 'process' | 'chat';
 
-export type ProgressStatus =
-  | 'idle'
-  | 'downloading_data'
-  | 'extracting_params'
-  | 'preprocessing'
-  | 'ai_analyzing'
-  | 'generating_response'
-  | 'complete'
-  | 'error';
-
-export interface ProgressDetail {
-  status: ProgressStatus;
-  message: string;
-  percentage?: number;
-  substeps?: string[];
-}
-
 export interface BioAgentStoreData {
   isLoading: boolean;
   extractedParams: PreprocessingParams | null;
@@ -31,7 +14,6 @@ export interface BioAgentStoreData {
   input: string;
   sessionId: string;
   isChatLoading: boolean;
-  progressDetail: ProgressDetail;
 }
 
 export interface BioAgentStoreState extends BioAgentStoreData {
